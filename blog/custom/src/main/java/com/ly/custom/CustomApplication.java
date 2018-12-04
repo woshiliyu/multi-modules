b8ab577b3e8168bb28cc62507e9ad5093d962cfc
woshiliyu <513422479@qq.com>
2018-12-04 10:39:20 +0800

    'asas'

---
+++ blog/custom/src/main/java/com/ly/custom/CustomApplication.java
@@ -0,0 +1,12 @@
+package com.ly.custom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomApplication.class, args);
+    }
+}
